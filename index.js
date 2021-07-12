844edae5c277474e6d385b1d59ee7b917a8a9bc6
osanc <hmax.bf4@gmail.com>
2021-07-12 17:46:39 -0600

    Code cleanup

--- index.js
+++ index.js
@@ -6,49 +6,45 @@
  */
 
 function funWithAnagrams(text) {
-  let remove = [];
+  let result = [];
+
+  function checkForAnagram(word) {
+    return result.some(r => {
+      if (r.length !== word.length) {
+        return false
+      }
+
+      return word.split('').sort().toString() === r.split('').sort().toString();
+    })
+  }
 
   for (let word of text) {
-    for (let i = 0; i < text.length; i++) {
-      if (word != text[i] && !remove.includes(word)) {
-        if (comp(word, text[i])) {
-          remove.push(text[i]);
-        }
-      }
+    if (!result.includes(word) && !checkForAnagram(word)) {
+      result.push(word)
     }
   }
 
-  return text.filter(n => !remove.includes(n)).sort();
+  return result
 }
 
-function comp(word, wordToComp) {
-  if (word.length != wordToComp.length) {
-    return false;
-  }
 
-  const arrWord = word.split("").sort();
-  const arrWordToComp = wordToComp.split("").sort();
-
-  return arrWord.toString() === arrWordToComp.toString();
-}
-
-console.log(funWithAnagrams(['code', 'aaagmnrs', 'anagrams', 'doce']));
+console.log(funWithAnagrams(['code', 'aaagmnrs', 'anagrams', 'doce', 'code', 'aaagmnrs', 'code']));
 console.log(funWithAnagrams(['poke', 'pkoe', 'okpe', 'ekop']));
-console.log(funWithAnagrams(["fqwltvzkqt", "volphckcyufdqmlglimklfzktgygdttnhcvpfdfbrp", "lkvshwywshtdgmbqbkkxcvgumo",
-  "mwvytbytnuqhmfjaqtgngcwkuzyamnerphfmwevh", "lezohyeehbrcewjxvceziftiqtntfsrptugtiznorvonzjfea",
-  "gamayapwlmbzitzszhzkosvnknber", "ltlkggdgpljfisyltmmfvhybljvk", "pcflsaqevcijcyrgmqirzniax",
-  "kholawoydvchveigttxwpukzjfh", "brtspfttotafsngqvoijxuvq", "ztvaalsehzxbshnrvbykjqlrzzfm",
-  "vyoshiktodnsjjpqplciklzqrxloqxrudygjty", "leizmeainxslwhhjwslqendjvx", "yghrveuvphknqtsdtwxcktmwwwsdthzmlmbh",
-  "kmouhpbqur", "fxgqlojmwsomowsjvpvhznbsilhhdkbdxqgrgedpzch", "gefeukmcowoeznwhpiiduxdnnlbnmyjyssbsococdzcu",
-  "nkrfduvouaghhcyvmlkza", "jpfpyljtyjjpyntsefxiswjuten", "ycpbcnmhfuqmmidmvknyxmywegmtunodvuzygvguxtrdsdf",
-  "fssmeluodjgdgzfmrazvndtaur", "kugs", "dpawxitivdubbqeonycaegxfjkkl", "fkraoheucsvpiteqrs",
-  "gkaaaohxxzhqjtkqaqhkwbe", "bpmglbjipnujywogwc", "lkyrdejaqufowbigrsnjniegvd",
-  "otugocedktcbbufnxorixibbdfrzuqsyrfqghoyqevcuanuu", "szitaoaowsxyglafbwzddoznrvjqeyqignpi",
-  "ruijvyllsibobjltusrypanvybsfrxtlfmpdidtyozoolz", "lgdgowijatklvjzscizrkupmsoxftumyxifyunxucubvk", "ctkqlr",
-  "qgzjvjwzizppvso", "flvioemycnphf", "tbnwedtubynsbirepgcxfgsfomhvpmymkdoh", "ttyyc", "ibbeaxniwjkfvabnrll",
-  "maglythkgla", "zgkeulyrpaeurdvexqlwgakdtbihmfrjijanxkhrqdllecy", "pcflsaqevcijcyrgmqixnzira", "klqrct",
-  "ibbeaxniwjkfvanrbll", "vyoshiktodnsjjpqplciklzqrxloqxrudyyjtg",
-  "ycpbcnmhfuqmmidmvknyxmywegmtunodvuzygvgxddftsru", "yyctt", "yghrveuvphknqtsdtwxcktmwwwsdtlhbhmmz",
-  "vyoshiktodnsjjpqplciklzqrxloqxrugyytjd", "cttyy", "brtspfttotafsngqvoiqxuvj", "lkyrdejaqufowbigrsnjvedgin",
-  "volphckcyufdqmlglimklfzktgygdttnhcvpfdrbpf", "qgzjvjwzizpsovp"
+console.log(funWithAnagrams(['fqwltvzkqt', 'volphckcyufdqmlglimklfzktgygdttnhcvpfdfbrp', 'lkvshwywshtdgmbqbkkxcvgumo',
+  'mwvytbytnuqhmfjaqtgngcwkuzyamnerphfmwevh', 'lezohyeehbrcewjxvceziftiqtntfsrptugtiznorvonzjfea',
+  'gamayapwlmbzitzszhzkosvnknber', 'ltlkggdgpljfisyltmmfvhybljvk', 'pcflsaqevcijcyrgmqirzniax',
+  'kholawoydvchveigttxwpukzjfh', 'brtspfttotafsngqvoijxuvq', 'ztvaalsehzxbshnrvbykjqlrzzfm',
+  'vyoshiktodnsjjpqplciklzqrxloqxrudygjty', 'leizmeainxslwhhjwslqendjvx', 'yghrveuvphknqtsdtwxcktmwwwsdthzmlmbh',
+  'kmouhpbqur', 'fxgqlojmwsomowsjvpvhznbsilhhdkbdxqgrgedpzch', 'gefeukmcowoeznwhpiiduxdnnlbnmyjyssbsococdzcu',
+  'nkrfduvouaghhcyvmlkza', 'jpfpyljtyjjpyntsefxiswjuten', 'ycpbcnmhfuqmmidmvknyxmywegmtunodvuzygvguxtrdsdf',
+  'fssmeluodjgdgzfmrazvndtaur', 'kugs', 'dpawxitivdubbqeonycaegxfjkkl', 'fkraoheucsvpiteqrs',
+  'gkaaaohxxzhqjtkqaqhkwbe', 'bpmglbjipnujywogwc', 'lkyrdejaqufowbigrsnjniegvd',
+  'otugocedktcbbufnxorixibbdfrzuqsyrfqghoyqevcuanuu', 'szitaoaowsxyglafbwzddoznrvjqeyqignpi',
+  'ruijvyllsibobjltusrypanvybsfrxtlfmpdidtyozoolz', 'lgdgowijatklvjzscizrkupmsoxftumyxifyunxucubvk', 'ctkqlr',
+  'qgzjvjwzizppvso', 'flvioemycnphf', 'tbnwedtubynsbirepgcxfgsfomhvpmymkdoh', 'ttyyc', 'ibbeaxniwjkfvabnrll',
+  'maglythkgla', 'zgkeulyrpaeurdvexqlwgakdtbihmfrjijanxkhrqdllecy', 'pcflsaqevcijcyrgmqixnzira', 'klqrct',
+  'ibbeaxniwjkfvanrbll', 'vyoshiktodnsjjpqplciklzqrxloqxrudyyjtg',
+  'ycpbcnmhfuqmmidmvknyxmywegmtunodvuzygvgxddftsru', 'yyctt', 'yghrveuvphknqtsdtwxcktmwwwsdtlhbhmmz',
+  'vyoshiktodnsjjpqplciklzqrxloqxrugyytjd', 'cttyy', 'brtspfttotafsngqvoiqxuvj', 'lkyrdejaqufowbigrsnjvedgin',
+  'volphckcyufdqmlglimklfzktgygdttnhcvpfdrbpf', 'qgzjvjwzizpsovp'
 ]));
